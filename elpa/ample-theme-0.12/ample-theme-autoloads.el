f5cbdde063aa42be518867a21150c8312efd7c8c
Lorenzo Farris <lofarris@microsoft.com>
2014-11-05 15:19:42 -0800

    moving emacs config around

---
+++ elpa/ample-theme-0.12/ample-theme-autoloads.el
@@ -0,0 +1,16 @@
+;;; ample-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("ample-theme.el") (21587 55379 160316
+;;;;;;  800000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ample-theme-autoloads.el ends here
---
+++ elpa/cygwin-mount-2001/cygwin-mount-autoloads.el
@@ -0,0 +1,16 @@
+;;; cygwin-mount-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("cygwin-mount.el") (21587 55375 845894
+;;;;;;  0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; cygwin-mount-autoloads.el ends here
